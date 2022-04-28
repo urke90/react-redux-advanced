9eafb970f98cf52c3e5219f7fcd7d1b77785ce66
urke90 <urosbijelic90@gmail.com>
2022-04-28 11:10:39 +0200

    setup redux store using toolkit

--- src/App.js
+++ src/App.js
@@ -12,3 +12,16 @@ function App() {
 }
 
 export default App;
+
+/**
+ * 1. click on My Cart btn in header should toggle shopping cart component
+ * 2. click on product will add product to the cart => if product exists then just increase quantity
+ * 3. if user clicks ( - ) on item in shopping cart decrease quantity ===> if q = 1 then remove from cart
+ */
+
+/**
+ * * STATE
+ * 1. showCart ===> boolean
+ * 2. cart ===> [] of products
+ * 3. if we have different products we will need ID to differentiate them ( add uuid or React 18 hook)
+ */
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -2,22 +2,24 @@ import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+    const { title, price, description, id } = props;
 
-  return (
-    <li className={classes.item}>
-      <Card>
-        <header>
-          <h3>{title}</h3>
-          <div className={classes.price}>${price.toFixed(2)}</div>
-        </header>
-        <p>{description}</p>
-        <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
-      </Card>
-    </li>
-  );
+    console.log('id', id);
+
+    return (
+        <li className={classes.item}>
+            <Card>
+                <header>
+                    <h3>{title}</h3>
+                    <div className={classes.price}>${price.toFixed(2)}</div>
+                </header>
+                <p>{description}</p>
+                <div className={classes.actions}>
+                    <button>Add to Cart</button>
+                </div>
+            </Card>
+        </li>
+    );
 };
 
 export default ProductItem;
