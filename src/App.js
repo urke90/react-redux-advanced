67475097193e78d9e3058257409d54db3d2700d8
urke90 <urosbijelic90@gmail.com>
2022-04-29 12:46:34 +0200

    added sendCartData async action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// import { cartActions } from './store/cart/cart';
+import { sendCartData } from './store/cart/cart';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+    const dispatch = useDispatch();
     const showCart = useSelector((state) => state.cartReducer.showCart);
+    const cart = useSelector((state) => state.cartReducer.items);
 
     return (
         <Layout>
